12d96ed32e75eeb2ddc36206e189b8c0fc248a80
moguzyildiz <moguzyildiz@gmail.com>
2018-03-26 21:16:52 +0000

    DGUserInterface.java created online with Bitbucket

---
+++ DGUserInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package functiongraph;
+
+/**
+ *
+ * @author YOGA
+ */
+public interface DGUserInterface 
+{    
+    public void processCommands();
+}
